5d1a473fd8d009c773d811b696d2c4a05337b679
Chainstrument <minitutoriel@gmail.com>
2024-09-10 12:34:39 +0200

    add card

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -9,12 +9,25 @@ function Button (props ){
              >{props.children}</button>
 }
 
+function ShoeCard(props){
+    return(
+        <div className='card bg-neutral text-neutral-content shadow-md'>
+            <picture>
+                <img 
+                className='h-32 w-full object-cover'
+                src={props.image} alt="shoe" />
+            </picture>
+            <div className="card-body">
+                <p className="card-title">{props.title}</p>
+            </div>
+        </div>
+
+    )
+}
+
 export default function Components   ()   {
   return <div className='flex flex-col items-start gap-4'>
-        <Button className="btn-xl" >
-            <Rabbit size={24} />
-            Le btn</Button>
-  
+       <ShoeCard image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" title="requin" />
   </div>
    
 }
