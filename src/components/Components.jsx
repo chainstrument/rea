1638f54049fb92efb8862f9c88cdc9895b159479
Chainstrument <minitutoriel@gmail.com>
2024-09-10 11:24:49 +0200

    add classname props

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Button (props ){
     console.log(props)
-    return <button className='btn btn-primary'>{props.name}</button>
+    return <button className={'btn btn-primary ' + props.className}>{props.name}</button>
 }
 
 export default function Components   ()   {
   return <div className='flex flex-col items-start gap-4'>
-        <Button name={"Bonjour"}/>
+        <Button className="btn-xl" name={"Bonjour"}/>
   
   </div>
    
