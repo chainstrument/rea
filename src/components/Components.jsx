65824f327833c0b6dd465e2cb823c86e8744eb77
Chainstrument <minitutoriel@gmail.com>
2024-09-10 12:10:18 +0200

    add lucid

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,13 +1,16 @@
+import { Rabbit } from 'lucide-react'
 import React from 'react'
 
 function Button (props ){
     console.log(props)
-    return <button className={'btn btn-primary ' + props.className}>{props.name}</button>
+    return <button className={'btn btn-primary ' + props.className}>{props.children}</button>
 }
 
 export default function Components   ()   {
   return <div className='flex flex-col items-start gap-4'>
-        <Button className="btn-xl" name={"Bonjour"}/>
+        <Button className="btn-xl" >
+            <Rabbit size={24} />
+            Le btn</Button>
   
   </div>
    
