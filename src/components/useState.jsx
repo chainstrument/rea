96bd4698b973e509206fb6a9ae91f14e22bcc8f0
Chainstrument <minitutoriel@gmail.com>
2024-09-10 15:37:32 +0200

    add click deleted btn

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -8,9 +8,23 @@ const handleClick = () => {
   return (
     <div className='flex align-items-left justify-content-center'>
         <button id="test_id" onClick={handleClick} className='btn btn-primary'>Click me</button>
-        
+        <Todo />
     </div>
   )
 }
+
+const Todo = () => {
+    return (
+        <div className="flex items-center gap-4">
+            <p>Faire ses courses</p>
+            <button 
+                onClick={() => {
+                    console.log('deleted')
+                }}
+                className='btn btn-error btn-sm'>X</button>
+        </div>
+    )
+}
+
 
 export default Test
