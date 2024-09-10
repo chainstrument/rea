50358aafab434da883c5b4ecc31840d8c8391579
Chainstrument <minitutoriel@gmail.com>
2024-09-10 18:43:01 +0200

    add todo handleform

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -2,24 +2,45 @@ import { Plus, Search } from 'lucide-react'
 import  { useState } from 'react'
 
 const Test = () => {
-    const [count, setCount] = useState(10)
+ 
+    const [todos, setTodos] = useState({
+        todo: "Faire les courses",
+        completed: false,
+    });
 
-    const [todo, setTodo] = useState('')
-
-    const handleClick = () => {
-        console.log('clicked')
-        setCount(count + 10)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget);
+        const todo = formData.get('todo');
+        
+    
+        const newTodo =  {
+            todo: todo,
+            completed: false,
+        }
+    
     }
-  return (
-    <div className='flex align-items-left justify-content-center'>
-        <button id="test_id" onClick={handleClick} className='btn btn-primary'>Click me</button>
-        {count}
 
-        <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Search" onChange={(e) => setTodo(e.target.value)} />
-        <Plus />
-        </label>
-        <p className='flex '>Todo : {todo}</p>
+
+    
+
+  return (
+    <div className='flex align-items-left justify-content-center dark'>
+        <form onSubmit={(e) => {
+            handleSubmit(e);
+        }}>
+           
+
+                <label className="input input-bordered flex items-center gap-2">
+                <input name='todo' type="text" className="grow" placeholder="Search"   />
+            
+                <button className="btn btn-primary btn-sm btn-outline" type='submit'>    
+                    <Plus size={16} />
+                </button>
+                </label>
+
+        </form>
+        
     </div>
   )
 }
