e495a4e294c7f88e5e4efbf0ff68fd49137b62c4
Chainstrument <minitutoriel@gmail.com>
2024-09-10 11:21:58 +0200

    add props

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
+function Button (props ){
+    console.log(props)
+    return <button className='btn btn-primary'>{props.name}</button>
+}
+
 export default function Components   ()   {
-  return <button className="btn btn-success">Hello World</button>
+  return <div className='flex flex-col items-start gap-4'>
+        <Button name={"Bonjour"}/>
+  
+  </div>
    
 }
